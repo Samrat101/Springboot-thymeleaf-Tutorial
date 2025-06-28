b1a3e56b6af40764ed9ddd968dbb3e5b223d90e9
Samrat101 <samratrao101@gmail.com>
2025-06-28 06:29:11 +0530

    Initial commit

---
+++ src/main/java/sandeep/yadav/springboot/ThymeleadSpringbootApplication.java
@@ -0,0 +1,13 @@
+package sandeep.yadav.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleadSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleadSpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sandeep/yadav/springboot/ThymeleadSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package sandeep.yadav.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleadSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
